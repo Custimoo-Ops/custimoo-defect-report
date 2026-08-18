e1d4a525b78101ecb859d204ed74fa5cb7385f87
Lars Kraemmer <lakr-macmini@hermes.home.arpa>
2026-08-18 14:07:52 +0200

    Disable caching for live defect report

--- server.py
+++ server.py
@@ -114,6 +114,12 @@ class H(http.server.SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, directory="/app", **kwargs)
 
+    def end_headers(self):
+        self.send_header("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0")
+        self.send_header("Pragma", "no-cache")
+        self.send_header("Expires", "0")
+        super().end_headers()
+
     def _cookies(self):
         out = {}
         for part in (self.headers.get("Cookie") or "").split(";"):
